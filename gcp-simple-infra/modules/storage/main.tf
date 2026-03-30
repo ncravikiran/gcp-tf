4168bff0811fea92c6fd8828c685718a15a8a939
ncravikiran <ncravikiran@users.noreply.github.com>
2026-03-30 13:28:52 +0000

    Fix Terraform formatting for gcp-simple-infra

--- gcp-simple-infra/modules/storage/main.tf
+++ gcp-simple-infra/modules/storage/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "bucket" {
-  name     = var.bucket_name
-  location = var.region
+  name                        = var.bucket_name
+  location                    = var.region
   uniform_bucket_level_access = true
 }
--- gcp-simple-infra/terraform.tfvars
+++ gcp-simple-infra/terraform.tfvars
@@ -1,6 +1,6 @@
 project_id = "project-9cb824fe-f85e-4763-8bb"
 # Change defaults here if needed
-vpc_name = "tf-vpc"
+vpc_name    = "tf-vpc"
 subnet_name = "my-tf-subnet"
-vm_name = "tf-vm"
+vm_name     = "tf-vm"
 bucket_name = "tf-bucket-test-rk"
