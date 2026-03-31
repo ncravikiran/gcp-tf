63a52b1559c7902a17f347158e39f3ed9b23f8a9
ncravikiran <ncravikiran@users.noreply.github.com>
2026-03-31 10:30:56 +0000

    updated the code

--- gcp-simple-infra/modules/storage/main.tf
+++ gcp-simple-infra/modules/storage/main.tf
@@ -1,5 +1,3 @@
-resource "google_storage_bucket" "bucket" {
-  name                        = var.bucket_name
-  location                    = var.region
-  uniform_bucket_level_access = true
+data "google_storage_bucket" "bucket" {
+  name = var.bucket_name
 }
