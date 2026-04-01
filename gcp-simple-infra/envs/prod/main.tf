4a019bd8f9292ed141efecbaf5ed328bf7ab1708
ncravikiran <ncravikiran@users.noreply.github.com>
2026-04-01 03:02:23 +0000

    updated the script

--- gcp-simple-infra/envs/prod/main.tf
+++ gcp-simple-infra/envs/prod/main.tf
@@ -11,6 +11,8 @@ module "compute" {
   zone         = var.zone
   network_name = module.networking.network_name
   subnet_name  = module.networking.subnet_name
+
+  metadata_startup_script = file("${path.module}/startup-script.sh")
 }
 
 module "storage" {
--- gcp-simple-infra/modules/compute/startup-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Update and install Node.js
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# Verify
-node --version
-npm --version
