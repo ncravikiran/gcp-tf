a114be8a22a4bab743ab4dc565db52cde0cc02d9
ncravikiran <38657025+ncravikiran@users.noreply.github.com>
2026-04-07 19:50:23 +0530

    Update main.tf

--- gcp-simple-infra/modules/compute/main.tf
+++ gcp-simple-infra/modules/compute/main.tf
@@ -18,6 +18,11 @@ resource "google_compute_instance" "vm" {
     }
   }
   metadata_startup_script = var.startup_script
+
+service_account {
+  email  = google_service_account.vm_sa.email
+  scopes = ["cloud-platform"]
+}
 }
 
 resource "google_service_account" "vm_sa" {
