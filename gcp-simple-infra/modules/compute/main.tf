53a1d81fbe1a2123b65e45dce5da3994457cacae
ncravikiran <38657025+ncravikiran@users.noreply.github.com>
2026-04-07 19:49:21 +0530

    Update main.tf

--- gcp-simple-infra/modules/compute/main.tf
+++ gcp-simple-infra/modules/compute/main.tf
@@ -24,3 +24,8 @@ resource "google_service_account" "vm_sa" {
   account_id   = "vm-secret-accessor"
   display_name = "VM Secret Manager Accessor"
 }
+
+resource "google_project_iam_member" "vm_sa_secret_access" {
+  role   = "roles/secretmanager.secretAccessor"
+  member = "serviceAccount:${google_service_account.vm_sa.email}"
+}
