56249083f77d5d949a2a17f93fe4dae92a2e46cf
ncravikiran <38657025+ncravikiran@users.noreply.github.com>
2026-04-08 09:03:42 +0530

    Create main.tf

---
+++ gcp-simple-infra/modules/database/main.tf
@@ -0,0 +1,36 @@
+resource "google_sql_database_instance" "postgres" {
+  name             = var.db_instance_name
+  database_version = "POSTGRES_14"
+  region           = var.region
+  project          = var.project_id
+
+  settings {
+    tier = "db-f1-micro" # QA/POC size
+    ip_configuration {
+      ipv4_enabled = true
+
+      dynamic "authorized_networks" {
+        for_each = var.authorized_networks
+        content {
+          name  = "vm-access"
+          value = authorized_networks.value
+        }
+      }
+    }
+  }
+
+  deletion_protection = false
+}
+
+resource "google_sql_database" "app_db" {
+  name     = var.db_name
+  instance = google_sql_database_instance.postgres.name
+  project  = var.project_id
+}
+
+resource "google_sql_user" "app_user" {
+  name     = var.db_user
+  instance = google_sql_database_instance.postgres.name
+  password = var.db_password
+  project  = var.project_id
+}
