6990b49cfe8c4b7150cdf032766f10720350e88a
ncravikiran <38657025+ncravikiran@users.noreply.github.com>
2026-04-23 17:19:56 +0530

    Update main.tf

--- gcp-simple-infra/envs/qa/main.tf
+++ gcp-simple-infra/envs/qa/main.tf
@@ -3,7 +3,7 @@
 ########################################
 
 module "networking" {
-  source      = "../../modules/networking"
+  source = "../../modules/networking"
 
   vpc_name    = var.vpc_name
   subnet_name = var.subnet_name
@@ -47,17 +47,17 @@ resource "time_sleep" "wait_for_db" {
 ########################################
 
 module "compute" {
-  source          = "../../modules/compute"
+  source = "../../modules/compute"
 
   vm_name         = var.vm_name
   project_id      = var.project_id
   vm_machine_type = var.vm_machine_type
   zone            = var.zone
 
-  network_name    = module.networking.network_name
-  subnet_name     = module.networking.subnet_name
+  network_name = module.networking.network_name
+  subnet_name  = module.networking.subnet_name
 
-  startup_script  = file("${path.module}/startup-script.sh")
+  startup_script = file("${path.module}/startup-script.sh")
 
   depends_on = [
     time_sleep.wait_for_db
@@ -69,7 +69,7 @@ module "compute" {
 ########################################
 
 module "storage" {
-  source      = "../../modules/storage"
+  source = "../../modules/storage"
 
   bucket_name = var.bucket_name
   region      = var.region
